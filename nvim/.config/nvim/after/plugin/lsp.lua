0c499b953ecac523a218d3a6b92b281853f0e52c
benleejc <benleejc@gmail.com>
2023-01-29 18:51:59 +0800

    Update cmp mappings

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -56,16 +56,13 @@ lsp.configure('pylsp', {
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<Tab>'] = cmp.mapping.confirm({select = true}),
-    ['<C-Space>'] = cmp.mapping.complete(),
-})
 
---cmp_mappings['<Tab>'] = nil
---cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+    mapping = cmp.mapping.preset.insert({
+        ['<TAB>'] = cmp.mapping.confirm({ select=true }),
+    })
+})
 
 lsp.set_preferences({
     suggest_lsp_servers = true,
