625581fea1162d2c25e046ed0668813a772aa5f5
benleejc <benleejc@gmail.com>
2023-03-26 16:52:41 +0800

    Add text wrap in set.lua

--- nvim/.config/nvim/lua/ben/set.lua
+++ nvim/.config/nvim/lua/ben/set.lua
@@ -4,7 +4,8 @@ vim.opt.guicursor = ""
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
+--vim.opt.textwidth = 80
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -13,10 +14,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
