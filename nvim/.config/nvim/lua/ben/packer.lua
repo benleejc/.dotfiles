1c4a103aaa281388146b67577791ec9e49ad7260
benleejc <benleejc@gmail.com>
2023-11-13 11:20:43 +0800

    Update telescope version in packer

--- nvim/.config/nvim/lua/ben/packer.lua
+++ nvim/.config/nvim/lua/ben/packer.lua
@@ -9,8 +9,7 @@ return require('packer').startup(function(use)
     use { 'folke/tokyonight.nvim' }
     -- use { 'ful1e5/onedark.nvim' }
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
